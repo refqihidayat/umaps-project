ca7093b9488429d6673d08c788e7509cea3d7bae
refqihidayat <124865040+refqihidayat@users.noreply.github.com>
2025-05-18 18:58:51 +0700

    Add files via upload
    update: versi terbaru index.html untuk tampilan responsive

---
+++ building.js
@@ -0,0 +1,74 @@
+var buildings = {
+  "A1": {
+    coords: [-7.965054439658924, 112.61762801147361],
+    info: "<b>Fakultas Psikologi</b><br>No Data"
+  },
+  "A2": {
+    coords: [-7.965535625342612, 112.61788533809612],
+    info: "<b>Bank BNI Cabang Pembantu UM</b><br>No Data"
+  },
+  "A3": {
+    coords: [-7.964477686736595, 112.61818160230813],
+    info: "<b>Gedung UKM 2</b><br>No Data"
+  },
+  "A4": {
+    coords: [-7.964648700767665, 112.61793951212385],
+    info: "<b>Panggung Trapesium</b><br>No Data"
+  },
+  "A5": {
+    coords: [-7.9648012720488355, 112.61831534443787],
+    info: "<b>Sekolah Autis</b><br>No Data"
+  },
+  "A6": {
+    coords: [-7.963922728930959, 112.6180021508443],
+    info: "<b>Fakultas Ilmu Sosial</b><br>No Data"
+  },
+  "A7": {
+    coords: [-7.963706446084548, 112.6181240424056],
+    info: "<b>Fakultas Ilmu Sosial</b><br>No Data"
+  },
+  "A8": {
+    coords: [-7.963538784884723, 112.61818837406295],
+    info: "<b>Fakultas Ilmu Sosial</b><br>No Data"
+  },
+  "A9": {
+    coords: [-7.963366093772602, 112.61829502917807],
+    info: "<b>Fakultas Ilmu Sosial</b><br>No Data"
+  },
+  "A10": {
+    coords: [-7.9630978355936035, 112.61846432301239],
+    info: "<b>Fakultas Ilmu Sosial</b><br>No Data"
+  },
+  "A11": {
+    coords: [-7.964278371743252, 112.61840472141046],
+    info: "<b>Gedung UKM 1</b><br>No Data"
+  },
+  "A12": {
+    coords: [-7.964104696929791, 112.61850443947485],
+    info: "<b>Koperasi Mahasiswa (KOPMA)</b><br>No Data"
+  },
+  "A13": {
+    coords: [-7.9637005905321, 112.61881620169602],
+    info: "<b>Sasana Budaya</b><br>No Data"
+  },
+  "A14": {
+    coords: [-7.963330537696128, 112.61886778000431],
+    info: "<b>Museum Pendidikan</b><br>No Data"
+  },
+  "A15": {
+    coords: [-7.9636899427337555, 112.6192284170801],
+    info: "<b>Posko Keamanan</b><br>No Data"
+  },
+  "A": {
+    coords: [-7.962560418803771, 112.6189125968195],
+    info: "<b>Graha Rektorat</b><br>No Data"
+  },
+  "A16": {
+    coords: [-7.961929807972241, 112.61913255888099],
+    info: "<b>Poliklinik UM</b><br>No Data"
+  },
+  "STADION": {
+    coords: [-7.958, 112.618],
+    info: "<b>Stadion</b>"
+  }
+};
---
+++ main.js
@@ -0,0 +1,94 @@
+// Inisialisasi peta
+var map = L.map('map').setView([-7.961357, 112.617876], 17);
+
+// Tambahkan tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 20,
+  minZoom: 16,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Variabel penampung marker sementara
+var tempMarker = null;
+
+// Load data GeoJSON garis jalan
+fetch('map.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: function (feature) {
+        return {
+          color: 'red',
+          weight: 0.75,
+          fillOpacity: 0.1
+        };
+      }
+    }).addTo(map);
+  });
+
+// Fungsi untuk mengisi daftar gedung
+function populateBuildingList() {
+  const listContainer = document.getElementById('building-list');
+  listContainer.innerHTML = '';
+
+  for (const key in buildings) {
+    const item = document.createElement('div');
+    item.className = 'building-item';
+    item.innerHTML = `<strong>${key}</strong> - ${buildings[key].info.split('<br>')[0].replace('<b>', '').replace('</b>', '')}`;
+
+    item.onclick = function () {
+      if (tempMarker) {
+        map.removeLayer(tempMarker);
+      }
+      map.setView(buildings[key].coords, 18);
+      tempMarker = L.marker(buildings[key].coords).addTo(map)
+        .bindPopup(buildings[key].info)
+        .openPopup();
+    };
+
+    listContainer.appendChild(item);
+  }
+}
+
+// Jalankan saat halaman dimuat
+document.addEventListener('DOMContentLoaded', function () {
+  populateBuildingList();
+});
+
+// Fungsi Sidebar
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  const toggleBtn = document.getElementById('sidebar-toggle');
+  const searchBox = document.getElementById('search-container');
+
+  sidebar.classList.toggle('hidden');
+
+  // Ubah arah panah
+  const isHidden = sidebar.classList.contains('hidden');
+  toggleBtn.innerHTML = isHidden ? '❯' : '❮';
+
+  // Geser posisi search box
+  if (isHidden) {
+    searchBox.style.left = '20px'; // saat sidebar tertutup
+  } else {
+    searchBox.style.left = '320px'; // saat sidebar terbuka
+  }
+}
+
+// Fungsi pencarian gedung
+function searchBuilding() {
+  const input = document.getElementById("search-input").value.toUpperCase();
+  if (buildings[input]) {
+    if (tempMarker) {
+      map.removeLayer(tempMarker);
+    }
+
+    const building = buildings[input];
+    map.setView(building.coords, 18);
+    tempMarker = L.marker(building.coords).addTo(map)
+      .bindPopup(building.info)
+      .openPopup();
+  } else {
+    alert("Gedung tidak ditemukan!");
+  }
+}
