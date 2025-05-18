354bb53212d58f02c0bb7b3d088f31a80b9ec7b8
refqihidayat <124865040+refqihidayat@users.noreply.github.com>
2025-05-18 20:35:41 +0700

    Update building.js

--- building.js
+++ building.js
@@ -71,8 +71,260 @@ var buildings = {
     coords: [-7.961679184991351, 112.61895466718165],
     info: "<b>Green House FMIPA</b><br>No Data"
   },
-  "STADION": {
-    coords: [-7.958, 112.618],
-    info: "<b>Stadion</b>"
-  }
+    "A18": {
+  coords: [-7.961318196589559, 112.62007888547082],
+  info: "<b>Outdoor Learning Space</b><br>No Data"
+  }, 
+  "A19": {
+    coords: [-7.9616479356605625, 112.61980792438503],
+    info: "<b>Gedung Kuliah Bersama 1</b><br>No Data"
+  }, 
+  "A20.": {
+    coords: [-7.960892485957043, 112.62035540688329],
+    info: "<b>Gedung Kuliah Bersama 2</b><br>No Data"
+  }, 
+  "A21": {
+    coords: [-7.960884373132178, 112.61951154249175],
+    info: "<b>Sekolah Pascasarjana</b><br>No Data"
+  }, 
+  "A22": {
+    coords: [-7.960096878212493, 112.62002529612369],
+    info: "<b>SMP Labolatorium UM</b><br>No Data"
+  }, 
+  "A23": {
+    coords: [-7.960250706394513, 112.62024525920556],
+    info: "<b>SMP Labolatorium UM</b><br>No Data"
+  }, 
+  "A24": {
+    coords: [-7.959836038989251, 112.62021921094588],
+    info: "<b>SMP Labolatorium UM</b><br>No Data"
+  }, 
+  "A25": {
+    coords: [-7.959962159074377, 112.62042663227135],
+    info: "<b>SMP Labolatorium UM</b><br>No Data"
+  },
+  "A26": {
+    coords: [-7.960143038318388, 112.62071556285562],
+    info: "<b>Gedung Probis</b><br>No Data"
+  }, 
+  "A27": {
+    coords: [-7.960632324440032, 112.62081080077843],
+    info: "<b>SD Labolatorium UM</b><br>No Data"
+  },
+  "A28": {
+    coords: [-7.960471685386895, 112.62093133627873],
+    info: "<b>SD Labolatorium UM</b><br>No Data"
+  }, 
+  "A29": {
+    coords: [-7.960225283298583, 112.62109609501688],
+    info: "<b>Gedung Probis</b><br>No Data"
+  }, 
+  "A30": {
+    coords: [-7.9600689051132845, 112.62114713327476],
+    info: "<b>Gedung Probis</b><br>No Data"
+  },
+  "A31": {
+    coords: [-7.959904628974017, 112.62071171313734],
+    info: "<b>Fakultas Vokasi</b><br>No Data"
+  },
+  "B1": {
+    coords: [-7.964324070307153, 112.61678172842721],
+    info: "<b>Gedung Ujian</b><br>No Data"
+  }, 
+  "B2": {
+    coords: [-7.964562473775462, 112.61716158876597],
+    info: "<b>Fakultas Kedokteran</b><br>No Data"
+  }, 
+  "B3": {
+    coords: [-7.964216898065127, 112.61653989873484],
+    info: "<b>Fakultas Kedokteran</b><br>No Data"
+  }, 
+  "B4": {
+    coords: [-7.963683471751509, 112.617337137567],
+    info: "<b>Fakultas Psikologi</b><br>No Data"
+  }, 
+  "B5": {
+    coords: [-7.963308473504927, 112.61687274755056],
+    info: "<b>UPT. PTIK UM</b><br>No Data"
+  }, 
+  "B6": {
+    coords: [-7.963394899999102, 112.61743463802891],
+    info: "<b>Kuliah Bersama Pascasarjana</b><br>No Data"
+  }, 
+  "B7": {
+    coords: [-7.963139890911504, 112.61758752394402],
+    info: "<b>Kuliah Bersama Pascasarjana</b><br>No Data"
+  }, 
+  "B8": {
+    coords: [-7.962911070215016, 112.617076451631],
+    info: "<b>Departemen Teknik Mesin dan Industri, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B9": {
+    coords: [-7.962678926443486, 112.61720190721027],
+    info: "<b>Departemen Teknik Sipil dan Perencanaan, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B10": {
+    coords: [-7.9628489472461945, 112.61776315584852],
+    info: "<b>Departemen Pendidikan Tata Boga dan Busana, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B11": {
+    coords: [-7.962370354112535, 112.61800720876865],
+    info: "<b>Gedung Kuliah Bersama, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B12": {
+    coords: [-7.9622259071451555, 112.61744124897096],
+    info: "<b>Departemen Teknik Elektro dan Informatika, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B13": {
+    coords: [-7.961796269706778, 112.61768184421477],
+    info: "<b>Departemen Teknik Mesin dan Industri, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B14": {
+    coords: [-7.962367884932329, 112.6183014081326],
+    info: "<b>KPRI</b><br>No Data"
+  }, 
+  "B15": {
+    coords: [-7.961991335039933, 112.61812563647639],
+    info: "<b>Pusat Sumber Belajar</b><br>No Data"
+  }, 
+  "B16": {
+    coords: [-7.961518486639979, 112.61784265657825],
+    info: "<b>Departemen Tata Boga dan Busana, Fakultas Teknik</b><br>No Data"
+  }, 
+  "B17": {
+    coords: [-7.961763686984915, 112.6183203959311],
+    info: "<b>Lembaga Pengembangan Pendidikan dan Pembelajaran UM</b><br>No Data"
+  }, 
+  "B18": {
+    coords: [-7.961266464486801, 112.61873323416165],
+    info: "<b>Labolatorium Bersama, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B19": {
+    coords: [-7.960770180255449, 112.61833284857506],
+    info: "<b></b>Departemen Kimia, Fakultas Matematika dan IPA<br>No Data"
+  }, 
+  "B20": {
+    coords: [-7.960423916473823, 112.61854645157949],
+    info: "<b>Gedung Administrasi Umum, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B21": {
+    coords: [-7.960974295791405, 112.61888605461353],
+    info: "<b>Departemen Biologi, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B22": {
+    coords: [-7.960656784111648, 112.61905273086312],
+    info: "<b></b>Departemen Fisika, Fakultas Matematika dan IPA<br>No Data"
+  }, 
+  "B23": {
+    coords: [-7.9599552178435955, 112.61871852706923],
+    info: "<b>Gedung Kuliah Bersama, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B24": {
+    coords: [-7.960337819396734, 112.61918574822157],
+    info: "<b>Departemen Matematika, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B25": {
+    coords: [-7.960030557523353, 112.61930965049295],
+    info: "<b>Gedung Kuliah dan Kegiatan Mahasiswa, Fakultas Matematika dan IPA</b><br>No Data"
+  }, 
+  "B26": {
+    coords: [-7.959636713656841, 112.61900333611621],
+    info: "<b>Penerbit - Percetakan UM</b><br>No Data"
+  }, 
+  "B27": {
+    coords: [-7.959238258228194, 112.61934934107963],
+    info: "<b>Garasi UM</b><br>No Data"
+  }, 
+  "B28": {
+    coords: [-7.9586845010641785, 112.6194210165718],
+    info: "<b>Asrama Putra Soka UM </b><br>No Data"
+  }, 
+  "B29": {
+    coords: [-7.958850524429063, 112.61973617613366],
+    info: "<b>Asrama Putra Soka UM</b><br>No Data"
+  }, 
+  "B30": {
+    coords: [-7.959115497300856, 112.62008572025573],
+    info: "<b>UPT. P2LP UM</b><br>No Data"
+  }, 
+  "B31": {
+    coords: [-7.959471875642474, 112.62025917362759],
+    info: "<b>UPT. LAB Pancila & UPT. LSP UM</b><br>No Data"
+  }, 
+  "B32": {
+  coords: [-7.959315149821965, 112.62000637542782],
+  info: "<b>Aula Kenanga</b><br>No Data"
+  }, 
+  "B33": {
+    coords: [-7.959258086090064, 112.61969760162255],
+    info: "<b>Asrama Putra Soka UM</b><br>No Data"
+  }, 
+  "B34": {
+    coords: [-7.95908522204915, 112.61947057048607],
+    info: "<b>Asrama Putra Soka UM</b><br>No Data"
+  }, 
+  "B35": {
+    coords: [-7.958838618672641, 112.61940587881928],
+    info: "<b>Asrama Putra Soka UM</b><br>No Data"
+  }, 
+  "B36": {
+    coords: [-7.958976426458438, 112.6193179957987],
+    info: "<b>Asrama Putra Soka UM</b><br>No Data"
+  }, 
+  "B37": {
+    coords: [],
+    info: "<b>Lapangan Badminton</b><br>No Data"
+  }, 
+  "C1": {
+    coords: [-7.963703300400858, 112.61645147908891],
+    info: "<b>HOTMA</b><br>No Data"
+  }, 
+  "C2": {
+    coords: [-7.963585823137695, 112.61597228522186],
+    info: "<b>Rusunawa Putra Lili UM</b><br>No Data"
+  }, 
+  "C3": {
+    coords: [-7.962195682650317, 112.6165862961568],
+    info: "<b>UPT. Perpustakaan UM</b><br>No Data"
+  }, 
+  "C4": {
+    coords: [],
+    info: "<b></b><br>No Data"
+  }, 
+  "C5": {
+    coords: [],
+    info: "<b></b><br>No Data"
+  }, 
+  "D7": {
+    coords: [],
+    info: "<b>Gedung Dekanat Fakultas Ekonomi dan Bisnis</b><br>No Data"
+  }, 
+  "D8": {
+    coords: [],
+    info: "<b>Gedung Kuliah Bersama, Fakultas Ekonomi dan Bisnis</b><br>No Data"
+  }, 
+  "D9": {
+    coords: [],
+    info: "<b></b>Gedung Kuliah Bersama, Fakultas Ekonomi dan Bisnis<br>No Data"
+  }, 
+  "D10": {
+    coords: [],
+    info: "<b>Gedung Kuliah Bersama, Fakultas Ekonomi dan Bisnis</b><br>No Data"
+  }, 
+  "D11": {
+    coords: [],
+    info: "<b>Gedung Kuliah Bersama, Fakultas Ekonomi dan Bisnis</b><br>No Data"
+  }, 
+  "D12": {
+    coords: [],
+    info: "<b></b><br>No Data"
+  }, 
+  "D13": {
+    coords: [],
+    info: "<b>Gedung ORMAWA, Fakultas Ekonomi dan Bisnis</b><br>No Data"
+  },
+    "STADION": {
+      coords: [-7.958, 112.618],
+      info: "<b>Stadion</b>"
+    }
 };
